b8c10d71b4f52a5d033e69cd1b2175bd00d791c8
Dmitrii Fediuk <dfediuk@gmail.com>
2024-04-20 02:02:27 +0100

    https://github.com/cabinetsbay/192.241.144.149/issues/2

--- certificate/openssl-req.sh
+++ certificate/openssl-req.sh
@@ -1,5 +1,10 @@
 # 2024-04-20
 # 1) "Update the TLS certificate": https://github.com/cabinetsbay/192.241.144.149/issues/2
-# 2) https://www.sectigo.com/faqs/detail/OpenSSL-Apache-w-mod-ssl-NGINX-OS-X/kA01N000000rgRb
+# 2) https://www.openssl.org/docs/man1.1.1/man1/openssl-req.html
 # 3) https://df.tips/tag/openssl-req
-openssl req -nodes -newkey rsa:2048 -keyout .key -out .csr
+# 4) https://www.sectigo.com/faqs/detail/OpenSSL-Apache-w-mod-ssl-NGINX-OS-X/kA01N000000rgRb
+openssl req \
+	-keyout .key \
+	-newkey rsa:2048 \
+	-nodes \
+	-out .csr
